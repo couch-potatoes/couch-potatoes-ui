ea2b31eca39823991c89b38c32b439d9d55f57ba
JMH <jheld135@gmail.com>
2017-03-25 11:34:02 -0500

    Refactor

--- src/views/ResetPassword.jsx
+++ src/views/ResetPassword.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class ResetPassword extends React.Component {
+class ResetPassword extends Component {
   constructor(props) {
     super(props);
     this.state = {
