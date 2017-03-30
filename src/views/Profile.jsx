f00a977ba8be4ffb59df17696109a632de7ac70a
Hein Htet Zaw <thanlone1995@gmail.com>
2017-03-29 19:59:28 -0500

    edit icon added

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,55 +1,63 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {List, ListItem} from 'material-ui';
+import EditorModeEdit from 'material-ui/svg-icons/editor/mode-edit';
 
 class Profile extends React.Component {
   render() {
     const {
       email
-    } = this.props;
+    }=this.props;
     return (
       <div id="profile-information">
         <div className="page-title">User Profile</div>
-        <List id="profile-list">
+        <List>
           <ListItem
-             primaryText = "Email"
-             secondaryText = {`${email}`}
+             primaryText="Email"
+             secondaryText={`${email}`}
           />
           <ListItem
-             primaryText = "ID"
-             secondaryText = '012345'
+             primaryText="ID"
+             secondaryText='012345'
           />
           <ListItem
-             primaryText = "Username"
-             secondaryText = 'someUsername'
+             primaryText="Username"
+             secondaryText='someUsername'
           />
           <ListItem
-             primaryText = "Age"
-             secondaryText = '21'
+             primaryText="Age"
+             secondaryText='21'
+             rightIcon={<EditorModeEdit />}
           />
           <ListItem
-             primaryText = "Grade"
-             secondaryText = 'A'
+             primaryText="Grade"
+             secondaryText='A'
+             rightIcon={<EditorModeEdit />}
           />
           <ListItem
-             primaryText = "Gender"
-             secondaryText = 'F'
+             primaryText="Gender"
+             secondaryText='F'
+             rightIcon={<EditorModeEdit />}
           />
           <ListItem
-             primaryText = "Height"
-             secondaryText = "6 ft. 1 in."
+             primaryText="Height"
+             secondaryText="6 ft. 1 in."
+             rightIcon={<EditorModeEdit />}
           />
           <ListItem
-             primaryText = "Weight"
-             secondaryText = '180 lb'
+             primaryText="Weight"
+             secondaryText='180 lb'
+             rightIcon={<EditorModeEdit />}
           />
           <ListItem
-             primaryText = "Sport"
-             secondaryText = 'Swimming'
+             primaryText="Sport"
+             secondaryText='Swimming'
+             rightIcon={<EditorModeEdit />}
           />
           <ListItem
-             primaryText = "Expected Graduation Date"
-             secondaryText = 'May 2019'
+             primaryText="Expected Graduation Date"
+             secondaryText='May 2019'
+             rightIcon={<EditorModeEdit />}
           />
         </List>
       </div>
@@ -58,7 +66,7 @@ class Profile extends React.Component {
 
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps=(state) => {
   const {
     auth: {
       email,
@@ -74,7 +82,7 @@ const mapStateToProps = (state) => {
       exp_grd        //expected graduation date
       */
     },
-  } = state;
+  }=state;
   return {
     email,
 
