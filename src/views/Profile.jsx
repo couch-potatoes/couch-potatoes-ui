c40dde91d5131d944f30bd543c9a66bcc72d7088
Hein Htet Zaw <thanlone1995@gmail.com>
2017-03-29 19:25:31 -0500

    user profile mocked data

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {List, ListItem} from 'material-ui';
 
 class Profile extends React.Component {
   render() {
@@ -7,9 +8,50 @@ class Profile extends React.Component {
       email
     } = this.props;
     return (
-      <div>
-        <h1>Profile</h1>
-        <h3>Email: {`${email}`}</h3>
+      <div id="profile-information">
+        <div className="page-title">User Profile</div>
+        <List id="profile-list">
+          <ListItem
+             primaryText = "Email"
+             secondaryText = {`${email}`}
+          />
+          <ListItem
+             primaryText = "ID"
+             secondaryText = '012345'
+          />
+          <ListItem
+             primaryText = "Username"
+             secondaryText = 'someUsername'
+          />
+          <ListItem
+             primaryText = "Age"
+             secondaryText = '21'
+          />
+          <ListItem
+             primaryText = "Grade"
+             secondaryText = 'A'
+          />
+          <ListItem
+             primaryText = "Gender"
+             secondaryText = 'F'
+          />
+          <ListItem
+             primaryText = "Height"
+             secondaryText = "6 ft. 1 in."
+          />
+          <ListItem
+             primaryText = "Weight"
+             secondaryText = '180 lb'
+          />
+          <ListItem
+             primaryText = "Sport"
+             secondaryText = 'Swimming'
+          />
+          <ListItem
+             primaryText = "Expected Graduation Date"
+             secondaryText = 'May 2019'
+          />
+        </List>
       </div>
     );
   }
@@ -20,10 +62,22 @@ const mapStateToProps = (state) => {
   const {
     auth: {
       email,
+  /*   name,
+      id,
+      username,
+      age,
+      grade,
+      gender, 
+      height, 
+      weight, 
+      sport,
+      exp_grd        //expected graduation date
+      */
     },
   } = state;
   return {
     email,
+
   };
 };
 
