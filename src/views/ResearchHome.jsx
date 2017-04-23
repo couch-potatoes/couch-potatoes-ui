aeb7fd9cb112339f6bc7d4c77f9dda3df81bb5ad
Hein Htet Zaw <thanlone1995@gmail.com>
2017-04-23 11:56:50 -0500

    data is in - need to use data in chart

--- src/views/ResearchHome.jsx
+++ src/views/ResearchHome.jsx
@@ -2,39 +2,61 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Chart } from 'react-google-charts';
-
 export class Test extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       options: {
-        title: 'Age vs. Weight comparison',
-        hAxis: { title: 'Age', minValue: 0, maxValue: 15 },
-        vAxis: { title: 'Weight', minValue: 0, maxValue: 15 },
-        legend: 'none',
+        title: 'Nutrition Chart',
+        hAxis: { title: 'Date'},
+        vAxis: { title: 'Amount'},
+        legend: 'all',
       },
-      data: [
-        ['Age', 'Weight'],
-        [8, 12],
-        [4, 5.5],
-        [11, 14],
-        [4, 5],
-        [3, 3.5],
-        [6.5, 7],
-      ],
+      data: this.getData(),
     };
   }
+  
   render() {
     return (
-      <Chart
-        chartType="ScatterChart"
-        data={this.state.data}
-        options={this.state.options}
-        graph_id="ScatterChart"
-        width="100%"
-        height="400px"
-        legend_toggle
-      />
+      <div id="Research-BarChart"> 
+        <Chart
+          chartType="LineChart"
+          data={this.state.data}
+          options={this.state.options}
+          graph_id="LineChart"
+          width="100%"
+          height="400px"
+          legend_toggle
+        />
+      </div>
+    );
+  }
+
+  getData(){
+    var db_data = [];
+    fetch('https://potatoes-db.herokuapp.com/db.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'chart=daily-average',
+    }).then(response => {
+      return response.json();
+    }).then(
+      return_data => {
+        var count = 0;
+        var objArray = Object.keys(return_data).map((row) => return_data[row]);
+        db_data[count] = ['Date', 'Calories', 'Carbs (g)', 'Fat (g)', 'Protein (g)'];
+        objArray.map((col) => {
+          count++;
+          db_data[count] = [col.date, parseInt(col.calories), parseInt(col.carbs), parseInt(col.proteins), parseInt(col.fats)];
+        });
+
+        console.log(db_data);
+        return db_data;
+      }
     );
   }
 }
